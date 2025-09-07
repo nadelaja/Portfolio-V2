7d9e347bb2e1aae9233240ce97498e9a71170787
nadelaja <64113627+nadelaja@users.noreply.github.com>
2025-09-07 00:50:21 -0400

    It's never over
    mostly small tweaks for mobile

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
             content: 'My tooltip!',
             placement: 'top',
             arrow: false,
-            content: 'Game Design | UX Design',
+            content: 'More About Me!',
             allowHTML: true,
         });
 
@@ -10,11 +10,19 @@
             content: 'My tooltip!',
             placement: 'top',
             arrow: false,
-            content: 'UX Research ',
+            content: 'Game Design | UX Design',
             allowHTML: true,
         });
 
         tippy('#c', {
+            content: 'My tooltip!',
+            placement: 'top',
+            arrow: false,
+            content: 'UX Research ',
+            allowHTML: true,
+        });
+
+        tippy('#d', {
             content: 'My tooltip!',
             placement: 'top',
             arrow: false,
@@ -23,7 +31,7 @@
             allowHTML: true,
         });
 
-        tippy('#d', {
+        tippy('#e', {
             content: 'My tooltip!',
             placement: 'top',
             arrow: false,
@@ -31,92 +39,10 @@
             allowHTML: true,
         });
 
-        tippy('#e', {
+        tippy('#f', {
             content: 'My tooltip!',
             placement: 'top',
             arrow: false,
             content: 'Digital Game | UI & UX Design',
             allowHTML: true,
         });
-
-/* const modal = document.getElementById("projectModal");
-const modalTitle = document.getElementById("modalTitle");
-const modalImage = document.getElementById("modalImage");
-const modalDescription = document.getElementById("modalDescription");
-
-const projectDetails = {
-    project1: {
-        title: "Project 1",
-        images: ["/placeholder", "/placeholder","/placeholder","/placeholder"],
-        description: "Description of Project 1. "
-    },
-    project2: {
-        title: "Project 2",
-        images: ["/placeholder", "/placeholder"],        
-        description: "Description of Project 2. "
-    },
-    project3: {
-        title: "Project 3",
-        images: ["/placeholder", "/placeholder","/placeholder","/placeholder"],
-        description: "Description of Project 3. "
-    },
-    project4: {
-        title: "Project 4",
-        images: ["/placeholder", "/placeholder", "/placeholder", "/placeholder"],
-        description: "Description of Project 4. "
-    },
-    illustration: {
-        title: "Illustration",
-        images: ["/illustration/2022/Birthday_Cake.JPEG", "Illustration/2022/Milk.jpg","/placeholder","/placeholder"],
-        description: "Description."
-    } 
-};
-
-function openModal(projectId) {
-    const project = projectDetails[projectId];
-    modalTitle.textContent = project.title;
-     */
-
-    // Clear previous images
-    //modalImageContainer.innerHTML = '';
-    
-    // Add new images
-/*     project.images.forEach(imageSrc => {
-        const img = document.createElement('img');
-        img.src = imageSrc;
-        img.alt = project.title;
-        img.className = 'modal-image';
-        modalImageContainer.appendChild(img);
-    });
-    
-    modalDescription.textContent = project.description;
-    modal.style.display = "block";
-}
-
-
-function closeModal() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        closeModal();
-    }
-} */
-//rotating arrow funtion
-/*const arrow = document.querySelector('.arrow');
-const mediaQuery = window.matchMedia('(max-width: 1024px)');
-
-function arrowSwitch(e) {
-    if (e.matches) {
-        arrow.style.transform = 'rotate(90deg)';
-    } else {
-        arrow.style.transform = 'rotate(0deg)';
-    }
-}
-
-// Run the handler once at the start
-arrowSwitch(mediaQuery);
-
-// Add the listener for future changes
-mediaQuery.addListener(arrowSwitch);*/
